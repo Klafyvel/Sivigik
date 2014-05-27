c38dca512252a73b7b9594149053dd24f35cd722
Klafyvel <klafyvel@gmail.com>
2014-05-27 22:10:14 +0200

    Changed editor style.

--- article/forms.py
+++ article/forms.py
@@ -1,11 +1,13 @@
-#-*-coding : utf-8-*-
+# -*- coding: utf-8 -*-
 from django import forms
 
 from home.models import Category
 
 class EditArticleForm(forms.Form):
-	title = forms.CharField(max_length=200)
-	image = forms.ImageField(required=False)
-	category = forms.ModelChoiceField(queryset=Category.objects.all())
-	is_beta = forms.BooleanField(help_text=u'Cette article est en beta.', required=False)
-	text = forms.CharField(widget=forms.Textarea)
+	error_css_class = 'error'
+
+	title = forms.CharField(max_length=200, label=u'Titre :')
+	image = forms.ImageField(required=False, label=u'Image :')
+	category = forms.ModelChoiceField(queryset=Category.objects.all(), label=u'Catégorie :')
+	is_beta = forms.BooleanField(label=unicode(u'Cette article est en bêta :'), required=False)
+	text = forms.CharField(widget=forms.Textarea, label='Texte :')
--- article/views.py
+++ article/views.py
@@ -12,6 +12,9 @@
 #
 #You should have received a copy of the GNU Affero General Public License
 #along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 
