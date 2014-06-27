46b5adac84cfde64b1bba84acff1133bf5af837a
Klafyvel <klafyvel@gmail.com>
2014-06-27 23:39:12 +0200

    mathjax will not be downloaded

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -2,12 +2,10 @@
 
 mkdir tmp
 cd tmp
-# get the js tools (mathjax and source-code highlight)
-wget https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML -O mathjax.js
+# get the js tools (source-code highlight)
 wget http://yandex.st/highlightjs/8.0/highlight.min.js
 wget http://highlightjs.org/static/styles/atelier-dune.dark.css
 
-cp mathjax.js ../article/static/article
 cp highlight.min.js ../article/static/article
 cp atelier-dune.dark.css ../article/static/article
 
