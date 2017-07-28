d61ed86f4b653010697f1502ce1a4171f1e7b125
Klafyvel <klafyvel@gmail.com>
2017-07-28 15:20:27 +0200

    Gestion de la db.

--- sivigik/settings.py
+++ sivigik/settings.py
@@ -83,12 +83,22 @@ WSGI_APPLICATION = 'sivigik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+            },
+        }
+    }
 
 
 # Password validation
